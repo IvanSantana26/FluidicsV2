90151dfd4101cff0b4e3c76b5ae61ad97a779913
IvanSantana26 <48782552+IvanSantana26@users.noreply.github.com>
2019-04-10 10:34:22 -0500

    Toolbar termiando en todas las activitys

--- app/src/main/java/com/example/fluidics/DescripcionDiox.java
+++ app/src/main/java/com/example/fluidics/DescripcionDiox.java
@@ -1,7 +1,9 @@
 package com.example.fluidics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import com.example.fluidics.adapter.Adaptador;
@@ -20,16 +22,28 @@ public class DescripcionDiox extends AppCompatActivity {
 
         Lista = new ArrayList<Datos>();
 
-        Lista.add(new Datos(1,"Lunes","01/04/2019","25gr",R.drawable.co));
-        Lista.add(new Datos(2,"Martes","02/04/2019","15gr",R.drawable.co));
-        Lista.add(new Datos(3,"Miercoles","03/04/2019","27gr",R.drawable.co));
-        Lista.add(new Datos(4,"Jueves","04/04/2019","6gr",R.drawable.co));
-        Lista.add(new Datos(5,"Viernes","05/04/2019","28gr",R.drawable.co));
-        Lista.add(new Datos(6,"Sabado","06/04/2019","8gr",R.drawable.co));
-        Lista.add(new Datos(7,"Domingo","07/04/2019","25gr",R.drawable.co));
+        Lista.add(new Datos(1,"Lunes","01/04/2019","25gr",R.drawable.comodif));
+        Lista.add(new Datos(2,"Martes","02/04/2019","15gr",R.drawable.comodif));
+        Lista.add(new Datos(3,"Miercoles","03/04/2019","27gr",R.drawable.comodif));
+        Lista.add(new Datos(4,"Jueves","04/04/2019","6gr",R.drawable.comodif));
+        Lista.add(new Datos(5,"Viernes","05/04/2019","28gr",R.drawable.comodif));
+        Lista.add(new Datos(6,"Sabado","06/04/2019","8gr",R.drawable.comodif));
+        Lista.add(new Datos(7,"Domingo","07/04/2019","25gr",R.drawable.comodif));
 
-        Adaptador miadaptador = new Adaptador(getApplicationContext(),Lista);
+        Adaptador miadaptador = new Adaptador(getApplicationContext(), Lista);
 
         ListaDatos.setAdapter(miadaptador);
+        setUpToolbar();
+    }
+
+    public void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(v->{
+            startActivity(new Intent(this, MetricsActivity.class));
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/fluidics/DescripcionO.java
+++ app/src/main/java/com/example/fluidics/DescripcionO.java
@@ -1,8 +1,10 @@
 package com.example.fluidics;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import com.example.fluidics.adapter.Adaptador;
@@ -23,18 +25,29 @@ public class DescripcionO extends AppCompatActivity {
 
         Lista = new ArrayList<Datos>();
 
-        Lista.add(new Datos(1, "Lunes", "01/04/2019", "25gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(2, "Martes", "02/04/2019", "15gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(3, "Miercoles", "03/04/2019", "27gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(4, "Jueves", "04/04/2019", "6gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(5, "Viernes", "05/04/2019", "28gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(6, "Sabado", "06/04/2019", "8gr", R.drawable.fotosintesis));
-        Lista.add(new Datos(7, "Domingo", "07/04/2019", "25gr", R.drawable.fotosintesis));
+        Lista.add(new Datos(1, "Lunes", "01/04/2019", "25gr", R.drawable.o2));
+        Lista.add(new Datos(2, "Martes", "02/04/2019", "15gr", R.drawable.o2));
+        Lista.add(new Datos(3, "Miercoles", "03/04/2019", "27gr", R.drawable.o2));
+        Lista.add(new Datos(4, "Jueves", "04/04/2019", "6gr", R.drawable.o2));
+        Lista.add(new Datos(5, "Viernes", "05/04/2019", "28gr", R.drawable.o2));
+        Lista.add(new Datos(6, "Sabado", "06/04/2019", "8gr", R.drawable.o2));
+        Lista.add(new Datos(7, "Domingo", "07/04/2019", "25gr", R.drawable.o2));
 
         Adaptador miadaptador = new Adaptador(getApplicationContext(), Lista);
 
         ListaDatos.setAdapter(miadaptador);
+        setUpToolbar();
     }
 
+    public void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(v->{
+            startActivity(new Intent(this, MetricsActivity.class));
+            finish();
+        });
+    }
 
 }
